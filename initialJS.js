8ba5dc652dfda6d4bb9fed42d40ce642c0ba5a50
Eric McCormick <Eric McCormick>
2017-02-06 15:05:51 -0500

    noting how the array.push would work in this

--- initialJS.js
+++ initialJS.js
@@ -2,17 +2,22 @@
 
 
 // Isnt' right but still a question to ask for later
-/* var fTemp = 0;
+/*
+var fTemp = 0;
 var cTemp = 0;
 
-var tempConversion (fTemp, cTemp) = function (fTemp, cTemp){
+var tempConversion = function (fTemp, cTemp){
   var cTempConverstion = (fTemp - 32) * (5/9);
-  var fTempConversion = (cTemp * 5/9) + 32;
+  var fTempConversion = (cTemp * 9/5) + 32;
   return [cTempConversion, fTempConversion];
-
 }
+
 console.log(tempConverstion[0]);
+Remember to use array.push in order to get this idea working
 */
+
+
+
 var temp = 0;
 var unit = "";
 
@@ -25,9 +30,9 @@ function tempConversion (temp, unit){
     temp = (temp * (9/5)) + 32;
     return temp;
   }
-  // else {
-  //  console.log("Not valid. Try again.");
-  //}
+   else {
+    console.log("Not valid. Try again.");
+  }
 }
 
 console.log(tempConversion(212,"C"));
